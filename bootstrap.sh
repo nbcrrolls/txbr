a88a937b2b488c57af3057278416a8b604f89722
Luca Clementi <luca.clementi@gmail.com>
2013-02-02 01:27:46 +0000

    need the qt-devel to compile IMOD

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# $Id: bootstrap.sh,v 1.2 2013/01/24 17:30:53 clem Exp $
+# $Id: bootstrap.sh,v 1.3 2013/02/02 01:27:46 clem Exp $
 #
 # @Copyright@
 # @Copyright@
@@ -13,4 +13,4 @@ install atlas
 install atlas-devel
 compile_and_install opt-scipy
 
-
+yum --enablerepo=base install qt-devel qt3-devel
