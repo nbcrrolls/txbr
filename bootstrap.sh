ab36a728a4398490d730b218c00ceb67c8d15745
Luca Clementi <luca.clementi@gmail.com>
2013-02-07 23:03:44 +0000

    first attempt to a boostrap function

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# $Id: bootstrap.sh,v 1.3 2013/02/02 01:27:46 clem Exp $
+# $Id: bootstrap.sh,v 1.4 2013/02/07 23:03:44 clem Exp $
 #
 # @Copyright@
 # @Copyright@
@@ -9,8 +9,36 @@
 
 . $ROLLSROOT/etc/bootstrap-functions.sh
 
-install atlas
-install atlas-devel
-compile_and_install opt-scipy
+YumCache=/var/cache/yum/
+YumCacheBase=$YumCache/base/packages/
+YumCacheEpel=$YumCache/epel/packages/
 
-yum --enablerepo=base install qt-devel qt3-devel
+BasePakg="qt3-devel gmp gmp-devel atlas atlas-devel t1lib dvipng"
+EpelPakg="cln cln-devel ginac ginac-devel cmake"
+
+
+
+
+mkdir -p RPMS/x86_64
+
+yum --enablerepo=base install $BasePakg
+for i in $BasePakg; do 
+	cp $YumCacheBase/$i*rpm  RPMS/x86_64/;
+done
+
+yum --enablerepo=base install $EpelPakg
+for i in $EpelPakg; do 
+        cp $YumCacheEpel/$i*rpm  RPMS/x86_64/;
+done
+
+
+compile_and_install opt-scipy
+compile_and_install opt-matplotlib
+compile_and_install opt-sympy
+compile_and_install opt-swiginac
+compile_and_install OpenCV
+compile_and_install txbr-fftw3
+compile_and_install txbr-fftw3f
+compile_and_install imod
+
+
