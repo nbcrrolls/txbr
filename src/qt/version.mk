7321dc2c60633723069c1d695ae10c295d778285
root <root@rocce-vm8.ucsd.edu>
2016-02-19 10:37:31 -0800

    qt is prereq for EPhantom.

---
+++ src/qt/version.mk
@@ -0,0 +1,9 @@
+PKGROOT         = /opt/txbr/qt
+ARCHIVENAME     = qt-everywhere-opensource-src
+NAME            = txbr-qt
+VERSION         = 4.8.5
+RELEASE         = 0
+
+TARBALL_POSTFIX = tar.gz
+RPM.EXTRAS = AutoReq:No
+
