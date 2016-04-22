aae3da0cea4f95e08279913c848f0d4dce10babd
nadyawilliams <nadya@sdsc.edu>
2016-04-21 17:42:53 -0700

    update imod to 4.7.12 Can compile now. Need to have prerequisite qt

--- src/imod/version.mk
+++ src/imod/version.mk
@@ -1,5 +1,10 @@
 PKGROOT		= /opt/txbr
 NAME    	= imod
-VERSION 	= 4.5.8
-RELEASE 	= 1
-TARBALL_POSTFIX	= 
+VERSION 	= 4.7.12
+ARCHIVENAME    	= $(NAME)_$(VERSION)_src
+RELEASE 	= 0
+TARBALL_POSTFIX	= tar.gz
+
+QTDIR = $(PKGROOT)/qt
+QMAKESPEC = linux-g++-64
+
