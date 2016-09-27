023b0ac1fe7280d023d009c408e8f83ce44a4213
nadyawilliams <nadya@sdsc.edu>
2016-09-27 15:24:36 -0700

    changes needed for modules

--- src/txbr/version.mk
+++ src/txbr/version.mk
@@ -1,8 +1,15 @@
-PKGROOT		= /opt/txbr
-NAME    	= txbr
-VERSION 	= v3.1.1
-RELEASE 	= 0
-TARBALL_POSTFIX	= tar.gz
+
+VERSION.MK.MASTER = txbr.mk
+VERSION.MK.MASTER.DIR = ..
+VERSION.MK.INCLUDE = txbr.version.mk
+
+include $(VERSION.MK.INCLUDE)
+
+PKGROOT         = /opt/txbr
+NAME            = txbr
+VERSION         = $(TXBRVER)
+RELEASE         = 0
+TARBALL_POSTFIX = tar.gz
 
 RPM.EXTRAS = AutoReq:No
 
